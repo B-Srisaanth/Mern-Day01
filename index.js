89bce84d0319c1be3f175f0833417218ba6c763c
srisaanthb-del <srisaanthb@gmail.com>
2025-12-05 10:27:25 +0530

    5.0

---
+++ index.js
@@ -0,0 +1,39 @@
+/*function Cli() {
+    const heading = document.getElementsByClassName("data")
+    heading[0].innerText = "life la oru vishayatha left hand la deal panna life um left hand la adikum";
+
+    heading[1].innerHTML = "<i>ethume namma kai la illa enna kathukormnu thaan mukkiyam</i>";
+
+    const arr = []
+    for (let i = 0; i <= 10; i++) {
+        arr.push(i);
+    }
+    heading[2].innerText = arr;
+}
+
+function save() {
+    const acti = document.activeElement.tagName;
+
+    const heading = document.getElementById("db");
+    heading.innerText = acti;
+}
+
+let a = 0;
+const heading = document.getElementById("db");
+heading.innerText = a;
+
+function cli() {
+    heading.innerText = ++a;
+}
+
+const arr = () => {
+    const heading = document.createElement("h1");
+    heading.innerText = "Srisaanth"
+    const cont = document.getElementById("container");
+    cont.append(heading);
+}*/
+
+const col = function () {
+    const div = document.getElementById("cont");
+    div.style.backgroundColor = "black";
+}
